e866e9c0efc0f8f01be45601ac1300fa7781b365
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-05-27 13:43:55 -0500

    [licensing] try to handle the third-party dependencies we have in the datadog-agent repo

--- lib/omnibus/project.rb
+++ lib/omnibus/project.rb
@@ -832,6 +832,26 @@ module Omnibus
     expose :license
 
     #
+    # Set or retrieve additional {#third_party_licenses} of the project.
+    #
+    # @example
+    #   third_party_licenses 'LiCENSES/third-party.csv'
+    #
+    # @param [String] val
+    #   the location to the CSV file with the additional third party license list.
+    #
+    # @return [String]
+    #
+    def third_party_licenses(val = NULL)
+      if null?(val)
+        @third_party_licenses || "Unspecified"
+      else
+        @third_party_licenses = val
+      end
+    end
+    expose :third_party_licenses
+
+    #
     # Set or retrieve the location of the {#license_file}
     # of the project.  It can either be a relative path inside
     # the project source directory or a URL.
